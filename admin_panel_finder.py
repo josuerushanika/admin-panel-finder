ca41e74eda049df6868fc1b85f7cd36a8736cd17
josue rushanika <100031798+josuerushanika@users.noreply.github.com>
2023-11-24 10:38:02 -0500

    Update admin_panel_finder.py

--- admin_panel_finder.py
+++ admin_panel_finder.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from urllib2 import Request, urlopen, URLError, HTTPError
+import urllib.request
+import urllib.error
 
 def Space(j):
 	i = 0
 	while i<=j:
-		print " ",
+		print(" ") ,
 		i+=1
 
 
 def findAdmin():
 	f = open("link.txt","r");
 	link = raw_input("Enter Site Name \n(ex : example.com or www.example.com ): ")
-	print "\n\nAvilable links : \n"
+	print("\n\nAvilable links : \n")
+	
 	while True:
 		sub_link = f.readline()
 		if not sub_link:
@@ -27,14 +29,15 @@ def findAdmin():
 		except URLError as e:
 			continue
 		else:
-			print "OK => ",req_link
+			print("OK => ", req_link)
+			
 
 def Credit():
-	Space(9); print "#####################################"
-	Space(9); print "#   +++ Admin Panel Finder v1 +++   #"
-	Space(9); print "#     Script by Illûmïnåté Ðëmøñ    #"
-	Space(9); print "#    Bangladesh Black Hat Hackers   #"
-	Space(9); print "#####################################"
+	Space(9); print("#####################################")
+	Space(9); print ("#   +++ Admin Panel Finder v1 +++   #")
+	Space(9); print ("#     Script by Illûmïnåté Ðëmøñ    #")
+	Space(9); print ("#    Bangladesh Black Hat Hackers   #")
+	Space(9); print ("#####################################")
 
 Credit()
 findAdmin()
